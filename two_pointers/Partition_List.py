5aa98fc3e216143c4b677f09a92eccb4b5fa4b2b
tsutsuku <tsutsuku@aliyun.com>
2018-04-09 17:35:14 +0800

    ##180409

---
+++ two_pointers/Partition_List.py
@@ -0,0 +1,34 @@
+#Question No.86 Partition List
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def partition(self, head, x):
+        """
+        :type head: ListNode
+        :type x: int
+        :rtype: ListNode
+        """
+        s = ListNode(0)
+        n = head
+        s.next ,p = n , n.next
+        while p:
+            if p.val < x:
+                n.next = p.next
+                s.next, p.next = p, s.next
+                p = n.next
+            else:
+                n, p = n.next, p.next
+
+        return s.next
+
+s = [1,2,4,1,8,1,4,3]
+node = ListNode(s[0])
+d = node
+for i in range(1, len(s)):
+    node.next = ListNode(s[i])
+    node = node.next
+
+Solution().partition(d, 2)
